e068b8475c837b706e347e80a027e8a9b958ae50
Your Name <you@example.com>
2022-02-17 14:39:41 +0530

    commit

--- App.js
+++
@@ -1,9 +0,0 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenvalue = "";
-for(item of buttons){
-    item.addEventListner('click',(e)=>{
-        let buttontext = e.target.innerText;
-        console.log("button text is" , buttontext);
-    })
-}
