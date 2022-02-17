56f4380968a0547b64cd5cacef8419c0efb0a703
Your Name <you@example.com>
2022-02-17 14:50:13 +0530

    commit

--- Cal.js
+++ Cal.js
@@ -1,28 +1,2 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
-let screenvalue = "";
-for(item of buttons){
-    item.addEventListener('click',(e)=>{
-         buttontext = e.target.innerText;
-         console.log("button text is" , buttontext);
-         if(buttontext == 'x'){
-             buttontext = '*';
-             screenvalue += buttontext;
-             screen.value = screenvalue;
-         }
-        if(buttontext == 'C'){
-             screenvalue = "";
-             screen.value = screenvalue;
-         }
-
-        else if(buttontext == '='){
-            screen.value = eval(screenvalue);
-        }
-        
-        else{
-            screenvalue += buttontext; 
-             screen.value = screenvalue;
-
-        }
-    })
-}
