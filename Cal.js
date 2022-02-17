c3b06ca6d6212b935e6d33909a4dd0726bfd545e
Your Name <you@example.com>
2022-02-17 14:54:15 +0530

    commit

--- Cal.js
+++ Cal.js
@@ -1,2 +1,6 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
+screen.addEventListener(('click'),(e)=>{
+    buttons = e.target.innerText;
+    console.log("button "+""+buttons+""+"is clicked");
+})
